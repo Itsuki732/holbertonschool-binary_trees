074968536042c21080846de467ea1cc8896e6e2b
Bryan <bryanmartel.po@gmial.com>
2025-12-09 16:36:04 +0000

    fix Betty coding style

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - Goes through a binary tree using post-order traversal
- * @tree: Pointer to the root node of the tree to traverse
- * @func: Pointer to a function to call for each node
- *
- * Description: If tree or func is NULL, function does nothing.
- */
+* binary_tree_postorder - Goes through a binary tree using post-order traversal
+* @tree: Pointer to the root node of the tree to traverse
+* @func: Pointer to a function to call for each node
+*
+* Description: If tree or func is NULL, function does nothing.
+*/
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
@@ -15,6 +15,5 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 
 	binary_tree_postorder(tree->left, func);
 	binary_tree_postorder(tree->right, func);
-    func(tree->n);
-}	
-
+	func(tree->n);
+}
